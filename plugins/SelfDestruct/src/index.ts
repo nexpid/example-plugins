c3cda71f1bbad017446f3b861cc83213253559d9
nexpid <60316309+nexpid@users.noreply.github.com>
2023-12-22 23:35:20 +0100

    feat(SelfDestruct): use onLoad

--- plugins/SelfDestruct/src/index.ts
+++ plugins/SelfDestruct/src/index.ts
@@ -1,4 +1,4 @@
 import { id } from "@vendetta/plugin";
 import { removePlugin } from "@vendetta/plugins";
 
-export const onUnload = () => removePlugin(id);
+export const onLoad = () => removePlugin(id);
